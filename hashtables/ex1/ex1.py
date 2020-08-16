8c5de98eed13ce5d77678d266602b5713ed3539b
Patrick Baldwin <patrickbaldwin614@gmail.com>
2020-08-16 19:24:55 -0400

    ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,16 @@
-def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
-
+def get_indices_of_item_weights(weights, _, limit):
+    indices_by_weight = {}
+    for (index, weight) in enumerate(weights):
+        if weight not in indices_by_weight:
+            indices_by_weight[weight] = [index]
+        else:
+            indices_by_weight[weight].append(index)
+    for (index, weight) in enumerate(weights):
+        target = limit - weight
+        if target in indices_by_weight:
+            for cur_index in indices_by_weight[target]:
+                if index is not cur_index:
+                    return (index, cur_index)\
+                        if max(index, cur_index) == index\
+                        else (cur_index, index)
     return None
