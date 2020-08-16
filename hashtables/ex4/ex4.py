1766c5647e1cbe81333088f3c48097c5ad8e4dca
Patrick Baldwin <patrickbaldwin614@gmail.com>
2020-08-16 19:37:35 -0400

    ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,10 +1,18 @@
-def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+def intersection(arrays):
+    counter = {}
+    for arr in arrays:
+        for item in arr:
+            if item in counter:
+                counter[item] += 1
+            else:
+                counter[item] = 1
+    return [key for (key, value) in counter.items() if value == len(arrays)]
 
-    return result
+def has_negatives(a):
+    return intersection([
+        [-item for item in a if item < 0],
+        [item for item in a if item > 0],
+    ])
 
 
 if __name__ == "__main__":
